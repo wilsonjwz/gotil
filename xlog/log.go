e98e3f675455be805f185b9f7af76804fa9a5817
Jerry <85411418@qq.com>
2020-09-30 16:46:53 +0800

    update xlog

--- xlog/log.go
+++ xlog/log.go
@@ -7,6 +7,60 @@ var (
 	errLog   xLogger = &ErrorLogger{}
 )
 
+var (
+	Reset = Color([]byte{27, 91, 48, 109})
+	// 标准
+	White   = Color([]byte{27, 91, 51, 48, 109}) // 白色
+	Red     = Color([]byte{27, 91, 51, 49, 109}) // 红色
+	Green   = Color([]byte{27, 91, 51, 50, 109}) // 绿色
+	Yellow  = Color([]byte{27, 91, 51, 51, 109}) // 黄色
+	Blue    = Color([]byte{27, 91, 51, 52, 109}) // 蓝色
+	Magenta = Color([]byte{27, 91, 51, 53, 109}) // 紫色
+	Cyan    = Color([]byte{27, 91, 51, 54, 109}) // 青色
+	// 高亮
+	WhiteBright   = Color([]byte{27, 91, 49, 59, 51, 48, 109})
+	RedBright     = Color([]byte{27, 91, 49, 59, 51, 49, 109})
+	GreenBright   = Color([]byte{27, 91, 49, 59, 51, 50, 109})
+	YellowBright  = Color([]byte{27, 91, 49, 59, 51, 51, 109})
+	BlueBright    = Color([]byte{27, 91, 49, 59, 51, 52, 109})
+	MagentaBright = Color([]byte{27, 91, 49, 59, 51, 53, 109})
+	CyanBright    = Color([]byte{27, 91, 49, 59, 51, 54, 109})
+	// 英文斜体
+	WhiteBevel   = Color([]byte{27, 91, 51, 59, 51, 48, 109})
+	RedBevel     = Color([]byte{27, 91, 51, 59, 51, 49, 109})
+	GreenBevel   = Color([]byte{27, 91, 51, 59, 51, 50, 109})
+	YellowBevel  = Color([]byte{27, 91, 51, 59, 51, 51, 109})
+	BlueBevel    = Color([]byte{27, 91, 51, 59, 51, 52, 109})
+	MagentaBevel = Color([]byte{27, 91, 51, 59, 51, 53, 109})
+	CyanBevel    = Color([]byte{27, 91, 51, 59, 51, 54, 109})
+	// 下划线
+	WhiteUnderLine   = Color([]byte{27, 91, 52, 59, 51, 48, 109})
+	RedUnderLine     = Color([]byte{27, 91, 52, 59, 51, 49, 109})
+	GreenUnderLine   = Color([]byte{27, 91, 52, 59, 51, 50, 109})
+	YellowUnderLine  = Color([]byte{27, 91, 52, 59, 51, 51, 109})
+	BlueUnderLine    = Color([]byte{27, 91, 52, 59, 51, 52, 109})
+	MagentaUnderLine = Color([]byte{27, 91, 52, 59, 51, 53, 109})
+	CyanUnderLine    = Color([]byte{27, 91, 52, 59, 51, 54, 109})
+	// 背景色
+	WhiteBg   = Color([]byte{27, 91, 55, 59, 51, 48, 109})
+	RedBg     = Color([]byte{27, 91, 55, 59, 51, 49, 109})
+	GreenBg   = Color([]byte{27, 91, 55, 59, 51, 50, 109})
+	YellowBg  = Color([]byte{27, 91, 55, 59, 51, 51, 109})
+	BlueBg    = Color([]byte{27, 91, 55, 59, 51, 52, 109})
+	MagentaBg = Color([]byte{27, 91, 55, 59, 51, 53, 109})
+	CyanBg    = Color([]byte{27, 91, 55, 59, 51, 54, 109})
+	// 删除线
+	WhiteDelLine   = Color([]byte{27, 91, 57, 59, 51, 48, 109})
+	RedDelLine     = Color([]byte{27, 91, 57, 59, 51, 49, 109})
+	GreenDelLine   = Color([]byte{27, 91, 57, 59, 51, 50, 109})
+	YellowDelLine  = Color([]byte{27, 91, 57, 59, 51, 51, 109})
+	BlueDelLine    = Color([]byte{27, 91, 57, 59, 51, 52, 109})
+	MagentaDelLine = Color([]byte{27, 91, 57, 59, 51, 53, 109})
+	CyanDelLine    = Color([]byte{27, 91, 57, 59, 51, 54, 109})
+)
+
+type Color string
+
 type xLogger interface {
 	logOut(format *string, args ...interface{})
 }
--- xlog/log_test.go
+++ xlog/log_test.go
@@ -7,10 +7,10 @@ import (
 
 func TestLog(t *testing.T) {
 	// default log
-	Info("info")
-	Debug("debug")
-	Warn("warning")
-	Error("error")
+	Info(White, "白色 info", Reset, WhiteBright, "高亮 info", Reset, "恢复默认颜色", WhiteDelLine, "删除线", Reset, WhiteUnderLine, "下划线", Reset, WhiteBevel, "斜体 info", Reset, WhiteBg, "背景", Reset)
+	Debug(Blue, "蓝色 debug", Reset, BlueBright, "高亮 debug", Reset, "恢复默认颜色", BlueDelLine, "删除线", Reset, BlueUnderLine, "下划线", Reset, BlueBevel, "斜体 debug", Reset, BlueBg, "背景", Reset)
+	Warn(Yellow, "黄色 warning", Reset, YellowBright, "高亮 warning", Reset, "恢复默认颜色", YellowDelLine, "删除线", Reset, YellowUnderLine, "下划线", Reset, YellowBevel, "斜体 warning", Reset, YellowBg, "背景", Reset)
+	Error(Red, "红色 error", Reset, RedBright, "高亮 error", Reset, "恢复默认颜色", RedDelLine, "删除线", Reset, RedUnderLine, "下划线", Reset, RedBevel, "斜体 error", Reset, RedBg, "背景", Reset)
 
 	fmt.Println()
 
