c43dbd23f91db421d46c8018a389c0df0ebbf3fb
Jerry <85411418@qq.com>
2020-10-26 09:44:27 +0800

    add duration

--- xtime/xtime.go
+++ xtime/xtime.go
@@ -47,18 +47,18 @@ func (t Time) ToDB() ([]byte, error) {
 	return []byte(unix.String()), nil
 }
 
-//// Duration be used json unmarshal string time, like 1s, 500ms.
-//type Duration time.Duration
-//
-//// UnmarshalText unmarshal text to duration.
-//func (d *Duration) UnmarshalText(text []byte) error {
-//	tmp, err := time.ParseDuration(string(text))
-//	if err == nil {
-//		*d = Duration(tmp)
-//	}
-//	return err
-//}
-//
+// Duration be used json unmarshal string time, like 1s, 500ms.
+type Duration time.Duration
+
+// UnmarshalText unmarshal text to duration.
+func (d *Duration) UnmarshalText(text []byte) error {
+	tmp, err := time.ParseDuration(string(text))
+	if err == nil {
+		*d = Duration(tmp)
+	}
+	return err
+}
+
 //// Shrink will decrease the duration by comparing with context's timeout duration
 //// and return new timeout\context\CancelFunc.
 //func (d Duration) Shrink(c context.Context) (Duration, context.Context, context.CancelFunc) {
