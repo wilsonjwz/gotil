319fb027ec9caa46b6ce15b1dec94d50844c664f
Jerry <85411418@qq.com>
2020-11-10 19:47:44 +0800

    fix

--- web/echo_test.go
+++ web/echo_test.go
@@ -1,49 +1,46 @@
 package web
 
 import (
-	"os"
-	"os/signal"
-	"syscall"
 	"testing"
-	"time"
 
-	"github.com/iGoogle-ink/gotil/limit"
 	"github.com/labstack/echo/v4"
 )
 
 func TestInitEcho(t *testing.T) {
-	c := &Config{
-		Port: ":2234",
-		Limit: &limit.Config{
-			Rate:       0, // 0 速率不限流
-			BucketSize: 100,
-		},
-	}
-	e := InitEcho(c)
-	e.Release()
-	e.Echo.Use(e.Logger())
-	e.Echo.Use(e.Recover())
+	// 需要测试请自行解开注释测试
 
-	initRouteE(e.Echo)
-
-	e.Start()
-
-	ch := make(chan os.Signal)
-	signal.Notify(ch, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
-	for {
-		si := <-ch
-		switch si {
-		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
-			time.Sleep(time.Second)
-			// todo something
-
-			time.Sleep(time.Second)
-			return
-		case syscall.SIGHUP:
-		default:
-			return
-		}
-	}
+	//c := &Config{
+	//	Port: ":2234",
+	//	Limit: &limit.Config{
+	//		Rate:       0, // 0 速率不限流
+	//		BucketSize: 100,
+	//	},
+	//}
+	//e := InitEcho(c)
+	//e.Release()
+	//e.Echo.Use(e.Logger())
+	//e.Echo.Use(e.Recover())
+	//
+	//initRouteE(e.Echo)
+	//
+	//e.Start()
+	//
+	//ch := make(chan os.Signal)
+	//signal.Notify(ch, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	//for {
+	//	si := <-ch
+	//	switch si {
+	//	case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+	//		time.Sleep(time.Second)
+	//		// todo something
+	//
+	//		time.Sleep(time.Second)
+	//		return
+	//	case syscall.SIGHUP:
+	//	default:
+	//		return
+	//	}
+	//}
 }
 
 func initRouteE(e *echo.Echo) {
