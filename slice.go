3a372bf6ad47f86c35c2a935dce51e6074c5abab
Jerry <85411418@qq.com>
2020-12-10 20:55:59 +0800

    fix FilterSlice

--- slice.go
+++ slice.go
@@ -13,3 +13,18 @@ func MergeSliceRemoveDuplicate(slice1, slice2 []int) (merged []int) {
 	}
 	return merged
 }
+
+// 过滤数组 去除src中item，在dst中存在的item
+// src[1,2,3,4,5]   dst[2,4,6,8]	result[1,3,5]
+func FilterSlice(src []int, dst []int) (result []int) {
+	aMap := make(map[int]struct{})
+	for _, v := range dst {
+		aMap[v] = struct{}{}
+	}
+	for _, v := range src {
+		if _, has := aMap[v]; !has {
+			result = append(result, v)
+		}
+	}
+	return result
+}
